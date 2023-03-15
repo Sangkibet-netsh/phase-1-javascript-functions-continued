670b9692811365d7961c2dc8b0651d84776d5259
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-15 13:38:49 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(myWork="roller-skate"){
+
+    return `This Saturday, I want to ${myWork}!`;
+
+}
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+
+ function wrapAdjective(myAdjective="*"){
+    return function ( action="a hardworker"){
+       return `You are ${myAdjective}${action}${myAdjective}!`;
+
+    
+    }
+}
